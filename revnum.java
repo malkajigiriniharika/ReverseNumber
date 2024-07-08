d5019f9465f15a163939dee7b32badf6adfab753
mniharika <121678468+malkajigiriniharika@users.noreply.github.com>
2024-07-08 13:06:19 +0530

    Add files via upload

---
+++ revnum.java
@@ -0,0 +1,28 @@
+  class revnum { 
+  
+    static int reverse(int n) 
+    { 
+        
+       int rev = 0; 
+        
+        int rem; 
+  
+        while (n > 0) { 
+            rem = n % 10; 
+            rev = (rev * 10) + rem; 
+            n = n / 10; 
+        } 
+  
+        return rev; 
+    } 
+  
+   
+    public static void main(String[] args) 
+    { 
+        int n = Integer.parseInt(args[0]); 
+  
+        System.out.print("Reversed Number is "
+                         + reverse(n)); 
+    } 
+}
+       
